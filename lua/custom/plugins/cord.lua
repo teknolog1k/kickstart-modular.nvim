be93392c9ec7fe06c8f8704cabefd084d342c9ad
teknolog1k <teknolog1kal@protonmail.com>
2026-04-29 09:41:52 -0500

    Added cord.lua Discord RPC

---
+++ lua/custom/plugins/cord.lua
@@ -0,0 +1,14 @@
+return {
+  'vyfor/cord.nvim',
+     ---@type CordConfig
+  opts = {
+    editor = {
+      client = 'neovim',
+      tooltip = 'I use neovim btw'
+    },
+    display = {
+      theme = 'atom',
+      flavor = 'dark'
+    }
+  }
+}
