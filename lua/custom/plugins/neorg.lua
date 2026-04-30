11b530df366115ae1a0054d8c3624e2e25ddad7d
teknolog1k <teknolog1kal@protonmail.com>
2026-04-30 11:53:24 -0500

    Fixed neorg treesitter support

--- lua/custom/plugins/neorg.lua
+++ lua/custom/plugins/neorg.lua
@@ -2,5 +2,20 @@ return {
     "nvim-neorg/neorg",
     lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
     version = "*", -- Pin Neorg to the latest stable release
+    dependencies = {
+        'nvim-neorg/tree-sitter-norg',
+        'nvim-neorg/tree-sitter-norg-meta',
+    }, -- Add nvim-treesitter here
     config = true,
+    opts = {
+        load = {
+            ["core.defaults"] = {},
+            ["core.concealer"] = {},
+            ["core.integrations.treesitter"] = {
+                config = {
+                    configure_parsers = true
+                }
+            }
+        }
+    }
 }
