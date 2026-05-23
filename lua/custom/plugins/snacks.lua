bb365e03420da52a60931618b4bf16bfeaed0fd5
teknolog1k <teknolog1kal@protonmail.com>
2026-05-23 13:34:42 -0500

    Added snacks' explorer and quickfile modules

---
+++ lua/custom/plugins/snacks.lua
@@ -0,0 +1,15 @@
+return -- lazy.nvim
+{
+  'folke/snacks.nvim',
+  ---@type snacks.Config
+  opts = {
+    quickfile = {
+      -- your quickfile configuration comes here
+      -- or leave it empty to use the default settings
+    },
+    explorer = {
+      -- your explorer configuration comes here
+      -- or leave it empty to use the default settings
+    },
+  },
+}
